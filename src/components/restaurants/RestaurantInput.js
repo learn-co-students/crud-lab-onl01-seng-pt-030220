66b0f41dbfb2278d0883ddcc2473cc9116bb053d
github username <github email address>
2020-12-05 17:37:38 +0000

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state={
+    text:''
+  }
+
+  onChange = event => {
+    this.setState({
+      text:event.target.value
+    })
+  }
+
+  onSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text) 
+    this.setState({
+      text:'' 
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+      <form onSubmit={(event)=> {this.onSubmit(event)}}>
+      <label>
+        Restaurant Name:
+        <input type="text" onChange={(event) => this.onChange(event)} value={this.state.text}/>
+      </label>
+      <input type="submit"/>
+    </form>
+
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,19 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
+
+export default function manageRestaurants(state = [] , action) {
+
+    switch(action.type){
+        case "ADD_RESTAURANT":
+        let newRestaurant = {
+            id: cuid(),
+            text:action.text
+        }
+        return [...state,newRestaurant]
+        
+
+        default:
+            return state
+    }
 
 }
